1de8a1cef9367c52919345853779ed6ec5acd0ce
Pavel Kurbatov <pasha74-2011@mail.ru>
2025-05-02 10:10:01 +0500

    swiperFinal

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
+const swipperPagination = document.querySelector('.swiper-pagination');
 
+ function swiperFunction(pagination){
   let swiper = new Swiper(".slider", {
     slidesPerView: 'auto',
     spaceBetween: 16,
     pagination: {
-      el: ".swiper-pagination",
+      el: pagination,
       clickable: true,
     },
     breakpoints: {
@@ -15,6 +17,30 @@
       }
     }
   });
+ }
+
+ swiperFunction(swipperPagination);
+
+  const btnRepaired = document.querySelector('.btn-functional__text2');
+  const imgBtnRepaired = document.querySelector('.btn-functional__arrow2');
+  const swiperRepaired = document.querySelector('.repaired-devices');
+
+  btnRepaired.addEventListener('click', ()=>{
+    if(btnRepaired.innerHTML === 'Показать все'){
+      btnRepaired.textContent = 'Cкрыть';
+      imgBtnRepaired.style.transform = 'rotate(180deg)';
+      imgBtnRepaired.style.backgroundPosition = 'right';
+      swiperRepaired.style.maxHeight = '100%';
+    } else{
+      btnRepaired.textContent = 'Показать все';
+      imgBtnRepaired.style.transform = 'rotate(0deg)';
+      imgBtnRepaired.style.backgroundPosition = 'left';
+      swiperRepaired.style.maxHeight = '160px'
+      
+    }
+
+  })
+
 
 
 
